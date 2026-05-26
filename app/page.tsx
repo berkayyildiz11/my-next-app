272e00eefd81ba3d325d36877c25dbc7fc6201b4
Berkay Yıldız <121243891+berkayyildiz11@users.noreply.github.com>
2026-05-26 04:27:01 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
-"use client"; // Required for Next.js App Router
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+"use client";
 import Hero from "@/components/Hero";
 import StockGrid from "@/components/StockGrid";
 
 export default function Home() {
-  useEffect(() => {
-    // Connect to your backend
-    const socket = io("http://localhost:4000");
-
-    socket.on("connect", () => {
-      console.log("Connected to backend", socket.id);
-    });
-
-    // Cleanup on unmount
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
 
   return (
           <div>
